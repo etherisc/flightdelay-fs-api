f05fd592fca38c086bf093370b819a9043433732
Christoph Mussenbrock <christoph@etherisc.com>
2020-01-27 13:34:10 +0100

    Add declineApplication, expirePolicy Endpoints, add monitoringdata support

--- src/io/routes/policyController.js
+++ src/io/routes/policyController.js
@@ -4,6 +4,7 @@ module.exports = ({ routerCommand, schemas, policyService }) => {
   // routerCommand.post('/policies/apply', schemas.applyForPolicySchema, policyService, 'applyForPolicy')
   routerCommand.post('/policies/contract', schemas.contractSchema, policyService, 'applyForPolicy')
   routerCommand.get('/policies/get_by_id', schemas.getPolicyByIdSchema, policyService, 'getPolicyById')
+  routerCommand.get('/policies', schemas.getPolicyByIdSchema, policyService, 'getPolicyById')
   routerCommand.post('/policies/underwritePolicy', schemas.underwritePolicySchema, policyService, 'underwritePolicy')
   routerCommand.post('/policies/declineApplication', schemas.declineApplicationSchema, policyService, 'declineApplication')
   routerCommand.post('/policies/expirePolicy', schemas.expirePolicySchema, policyService, 'expirePolicy')
