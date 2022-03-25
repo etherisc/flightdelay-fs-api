0c6e5e873f5b74dd4e2fed27f763921eefa8cff7
Christoph Mussenbrock <christoph@etherisc.com>
2022-03-25 14:51:18 +0100

    Filter allowed/denied countries, airports, airlines...

--- src/services/flightStatsService.js
+++ src/services/flightStatsService.js
@@ -14,7 +14,7 @@ module.exports = class FlightStatsService {
    * @param telegramBot TelegramBot Object
    */
 
-  constructor({ config, telegramBot }) {
+  constructor ({config, telegramBot}) {
     this.config = config
     this.tg = telegramBot
     this.appId = config.APP_ID
@@ -22,7 +22,7 @@ module.exports = class FlightStatsService {
     this.httpProvider = config.HTTP_PROVIDER
     this.flightDelayContractAddressDemo = config.FLIGHTDELAY_ADDRESS_DEMO
     this.flightDelayContractAddressProduction = config.FLIGHTDELAY_ADDRESS_PRODUCTION
-    this.provider = new ethers.providers.JsonRpcProvider({ url: this.httpProvider })
+    this.provider = new ethers.providers.JsonRpcProvider({url: this.httpProvider})
 
     this.flightStatsBaseURL = 'https://api.flightstats.com'
     this.flightScheduleEndpoint = '/flex/schedules/rest/v1/json/flight'
@@ -40,12 +40,12 @@ module.exports = class FlightStatsService {
     )
   }
 
-  async fetchEndpoint(endpoint) {
+  async fetchEndpoint (endpoint) {
     const response = await fetch(endpoint)
     return response.json()
   }
 
-  async getFlightStats(ctx, endpoint) {
+  async getFlightStats (ctx, endpoint) {
     const json = await this.fetchEndpoint(endpoint)
     if (json.error) {
       await this.tg.send(`Error: ${JSON.stringify(json.error)}`)
@@ -56,7 +56,7 @@ module.exports = class FlightStatsService {
     }
   }
 
-  async getFlightStatsOracle(ctx, endpoint) {
+  async getFlightStatsOracle (ctx, endpoint) {
     const json = await this.fetchEndpoint(endpoint)
     if (json.error) {
       await this.tg.send(`Error: ${JSON.stringify(json.error)}`)
@@ -65,7 +65,7 @@ module.exports = class FlightStatsService {
     return json
   }
 
-  getScheduleEndpoint(data) {
+  getScheduleEndpoint (data) {
     const {
       carrier, flightNumber, year, month, day,
     } = data
@@ -77,7 +77,7 @@ departing/${year}/${month}/${day}\
 `
   }
 
-  getStatusEndpoint(data) {
+  getStatusEndpoint (data) {
     const {
       carrier, flightNumber, year, month, day,
     } = data
@@ -89,8 +89,8 @@ ${carrier}/${flightNumber}\
 `
   }
 
-  getStatusOracleEndpoint(data) {
-    const { carrierFlightNumber, yearMonthDay } = data
+  getStatusOracleEndpoint (data) {
+    const {carrierFlightNumber, yearMonthDay} = data
     return `\
 ${this.flightStatsBaseURL}${this.flightStatusEndpoint}/\
 ${carrierFlightNumber}\
@@ -99,8 +99,8 @@ ${carrierFlightNumber}\
 `
   }
 
-  getRatingsEndpoint(data) {
-    const { carrier, flightNumber } = data
+  getRatingsEndpoint (data) {
+    const {carrier, flightNumber} = data
     return `\
 ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
 /${carrier}/${flightNumber}\
@@ -108,8 +108,8 @@ ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
 `
   }
 
-  getRatingsOracleEndpoint(data) {
-    const { carrierFlightNumber } = data
+  getRatingsOracleEndpoint (data) {
+    const {carrierFlightNumber} = data
     return `\
 ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
 /${carrierFlightNumber}\
@@ -117,30 +117,30 @@ ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
 `
   }
 
-  async getSchedule(ctx, data) {
+  async getSchedule (ctx, data) {
     await this.tg.send(`Get Schedule: ${JSON.stringify(data)}`)
     await this.getFlightStats(ctx, this.getScheduleEndpoint(data))
   }
 
-  async getStatus(ctx, data) {
+  async getStatus (ctx, data) {
     await this.tg.send(`Get Status: ${JSON.stringify(data)}`)
     await this.getFlightStats(ctx, this.getStatusEndpoint(data))
   }
 
-  async getRatings(ctx, data) {
+  async getRatings (ctx, data) {
     await this.tg.send(`Get Ratings: ${JSON.stringify(data)}`)
     await this.getFlightStats(ctx, this.getRatingsEndpoint(data))
   }
 
-  async getStatusOracle(ctx, data) {
+  async getStatusOracle (ctx, data) {
     await this.tg.send(`Get Status Oracle: ${JSON.stringify(data)}`)
     const statusHex = (str) => `0x${str.charCodeAt(0).toString(16)}`
-    const revertResult = { status: statusHex('X'), arrived: false, delay: 0 } // this will lead to a revert in the smart contract
+    const revertResult = {status: statusHex('X'), arrived: false, delay: 0} // this will lead to a revert in the smart contract
     let result
 
     const checkSpecialFlights = async (data) => {
       try {
-        const specialFlights = fs.read('src/data/specialFlights.json', 'json')
+        const specialFlights = fs.read('./src/data/specialFlights.json', 'json')
         const specialResult = specialFlights.filter(item => JSON.stringify(item.key) === JSON.stringify(data))
         if (specialResult.length === 0) {
           const msg = 'Error: result has no flightStatuses'
@@ -167,7 +167,7 @@ ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
         'status' in flightStatuses
         && 'operationalTimes' in flightStatuses
       ) {
-        const { status: statusString } = flightStatuses
+        const {status: statusString} = flightStatuses
         const status = statusHex(statusString)
         if (status === statusHex('L')) {
           let arrived = 'actualGateArrival' in flightStatuses.operationalTimes
@@ -183,13 +183,13 @@ ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
             const delay = 'delays' in flightStatuses && 'arrivalGateDelayMinutes' in flightStatuses.delays
               ? flightStatuses.delays.arrivalGateDelayMinutes
               : 0
-            result = { status, arrived, delay }
+            result = {status, arrived, delay}
           } else {
             // landed, but no actualGateArrival or actualRunwayArrival, so probably taxiing or doors not open
-            result = { status: statusHex('A'), arrived: false, delay: 0 }
+            result = {status: statusHex('A'), arrived: false, delay: 0}
           }
         } else {
-          result = { status, arrived: false, delay: 0 }
+          result = {status, arrived: false, delay: 0}
         }
       }
       await this.tg.send(`Result: ${JSON.stringify(result)}`)
@@ -200,33 +200,68 @@ ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
     }
   }
 
-  async getRatingsOracle(ctx, data) {
+  async getRatingsOracle (ctx, data) {
+
+    const isAllowed = (flight) => {
+      const {airlineFsCode, departureAirportFsCode, arrivalAirportFsCode} = flight.ratings[0]
+      console.log(airlineFsCode, departureAirportFsCode, arrivalAirportFsCode)
+      const departureAirport = flight.appendix.airports.filter(item => item.fs === departureAirportFsCode)[0]
+      const arrivalAirport = flight.appendix.airports.filter(item => item.fs === arrivalAirportFsCode)[0]
+      const departureCountry = departureAirport.countryCode
+      const arrivalCountry = arrivalAirport.countryCode
+      const filterList = fs.read('./src/data/filterList.json', 'json')
+      const isOk = (property, departure, arrival) => {
+        const {order, list} = filterList[property]
+        return (order === 'allow'
+          ? list.includes(departure) && list.includes(arrival)
+          : !(list.includes(departure) || list.includes(arrival)))
+      }
+      console.log(
+        `Country: ${isOk('countries', departureCountry, arrivalCountry) ? 'ok' : 'not ok'}, \
+Airports: ${isOk('airports', departureAirportFsCode, arrivalAirportFsCode) ? 'ok' : 'not ok'}, \
+Airlines: ${isOk('airlines', airlineFsCode, airlineFsCode) ? 'ok' : 'not ok'}`
+      )
+      return (
+        isOk('countries', departureCountry, arrivalCountry) &&
+        isOk('airports', departureAirportFsCode, arrivalAirportFsCode) &&
+        isOk('airlines', airlineFsCode, airlineFsCode)
+      )
+    }
+
+    const noFlight = () => ctx.ok({
+        observations: 0, late15: 0, late30: 0, late45: 0, cancelled: 0, diverted: 0,
+      })
+
     await this.tg.send(`Get Ratings Oracle: ${JSON.stringify(data)}`)
     try {
       const json = await this.getFlightStatsOracle(ctx, this.getRatingsOracleEndpoint(data))
-      const ratings = json.ratings[0]
-      const result = {
-        observations: ratings.observations,
-        late15: ratings.late15,
-        late30: ratings.late30,
-        late45: ratings.late45,
-        cancelled: ratings.cancelled,
-        diverted: ratings.diverted,
-      }
+      if (isAllowed(json)) {
+        const ratings = json.ratings[0]
+        const result = {
+          observations: ratings.observations,
+          late15: ratings.late15,
+          late30: ratings.late30,
+          late45: ratings.late45,
+          cancelled: ratings.cancelled,
+          diverted: ratings.diverted,
+        }
 
-      await this.tg.send(`Result: ${result}`)
-      ctx.ok(result)
+        await this.tg.send(`Result: ${result}`)
+        ctx.ok(result)
+      } else {
+        await this.tg.send(`Result: Country/Airport/Airline not allowed`)
+        noFlight()
+      }
     } catch (error) {
-      ctx.ok({
-        observations: 0, late15: 0, late30: 0, late45: 0, cancelled: 0, diverted: 0,
-      })
+      await this.tg.send(`Result: Error, ${error.message}`)
+      noFlight()
     }
   }
 
-  async getQuote(ctx, data) { // data = { premium, carrier, flightNumber }
+  async getQuote (ctx, data) { // data = { premium, carrier, flightNumber }
     await this.tg.send(`Get Quote: ${JSON.stringify(data)}`)
-    const { ratings } = await this.fetchEndpoint(
-      this.getRatingsEndpoint({ carrier: data.carrier, flightNumber: data.flightNumber }),
+    const {ratings} = await this.fetchEndpoint(
+      this.getRatingsEndpoint({carrier: data.carrier, flightNumber: data.flightNumber}),
     )
     if (ratings && ratings[0] && ratings[0].observations) {
       const rating = (({
@@ -234,7 +269,7 @@ ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
       }) => ({
         observations, ontime, late15, late30, late45, cancelled, diverted,
       }))(ratings[0])
-      const { premium } = data // here premium is in USD Cents
+      const {premium} = data // here premium is in USD Cents
       const premiumInWei = ethers.utils.parseEther((parseInt(premium, 10) / 100).toFixed(2).toString())
       const parameters = [
         premiumInWei,
@@ -257,7 +292,7 @@ ${this.flightStatsBaseURL}${this.flightRatingsEndpoint}\
         payoutOptions: result._payoutOptions.map((item) => ethers.utils.formatUnits(item, 0)),
       }
       await this.tg.send(` Ratings: ${JSON.stringify(rating)} \n Quote: ${JSON.stringify(quote)}`)
-      ctx.ok({ rating, quote })
+      ctx.ok({rating, quote})
     } else {
       await this.tg.send(` Flight not available for ${data.carrier} ${data.flightNumber}`)
       ctx.status = 404
